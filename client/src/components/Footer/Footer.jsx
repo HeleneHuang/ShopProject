a4672e2479b9e4fcb22243bf0080e8f24f311133
HeleneHuang <helene568363@gmail.com>
2025-06-23 00:43:32 +0200

    completed footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import "./Footer.scss";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <div className="footer">
 
-export default Footer
+            <div className="top">
+                <div className="item">
+                    <h1>Categories</h1>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+                <div className="item">
+                    <h1>Links</h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h1>About</h1>
+                    <span>
+                        Welcome to Helene's Shop, your go-to destination for stylish and unique fashion finds!
+                        <br />This is a demo e-commerce website showcasing all the trendy clothes Helene loves.
+                        <br />Every item reflects a passion for style, creativity, and individuality.
+                    </span>
+                </div>
+                <div className="item">
+                    <h1>Contact</h1>
+                    <span>
+                        Have questions, suggestions, or just want to say hi?
+                        We'd love to hear from you!
+                        <br />Email: support@heleneshop.com
+                        <br />Location: Stockholm, Sweden
+                        <br />Instagram: @helenes.fashion
+                        <br />Business Hours: Mon–Fri, 9:00 AM – 6:00 PM (CET)
+                    </span>
+                </div>
+            </div>
+
+            <div className="bottom">
+                <div className="left">
+                    <span className="logo">Helene's Shop</span>
+                    <img src='assets/img/logo.png' />
+                    <span className="copyright">
+                        © Copyright 2025. All Rights Reserved
+                    </span>
+                </div>
+                <div className="right">
+                    <img src="/img/payment.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
