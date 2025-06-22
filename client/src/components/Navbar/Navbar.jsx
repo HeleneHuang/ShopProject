d5ade7654c6fcd352556c8f8ba0c5ddf013385f9
HeleneHuang <helene568363@gmail.com>
2025-06-23 00:31:15 +0200

    complete navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+// import React, { useState } from "react";
+// import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./Navbar.scss"
 
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+    // const [open, setOpen] = useState(false)
+    // const products = useSelector((state) => state.cart.products);
 
-export default Navbar
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+
+                <div className="left">
+                    <div className="item">
+                        <img src="assets/img/en.png" />
+                        <img src="assets/img/down.png" />
+                    </div>
+                    <div className="item">
+                        <span>USD</span>
+                        <img src="assets/img/down.png" />
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/1">Women</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/2">Men</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/3">Children</Link>
+                    </div>
+                </div>
+
+                <div className="center">
+                    <Link className="link" to="/">Helene's Shop</Link>
+                </div>
+
+                <div className="right">
+                    <div className="item">
+                        <Link className="link" to="/">Homepage</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">About</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Contact</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Stores</Link>
+                    </div>
+                    <div className="icons">
+                        <img src="assets/img/search.png" />
+                        <img src="assets/img/person.png" />
+                        <img src="assets/img/like.png" />
+
+                        <div className="cartIcon">
+                            <img src="assets/img/cart.png" />
+                            <span>0</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            {/* {open && <Cart />} */}
+        </div>
+    );
+};
+
+export default Navbar;
