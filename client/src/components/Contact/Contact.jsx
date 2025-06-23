7f3f0403e585c3e3d864b0d5a72f4cb5153fff86
HeleneHuang <helene568363@gmail.com>
2025-06-23 13:08:06 +0200

    finished contact

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Contact.scss";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
     return (
-        <div>Contact</div>
-    )
-}
+        <div className="contact">
+            <div className="wrapper">
+                <span>BE IN TOUCH WITH US:</span>
+                <div className="mail">
+                    <input type="text" placeholder="Enter your e-mail..." />
+                    <button>JOIN US</button>
+                </div>
 
-export default Contact
+                <div className="icons">
+                    <img src="assets/img/Facebook.png" />
+                    <a href="https://github.com/HeleneHuang/ShopProject" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/github.png" alt="GitHub" />
+                    </a>
+                    <img src="assets/img/ins.png" />
+                    <img src="assets/img/linkedin.png" />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Categories from '../../components/Categories/Categories'
-// import Contact from '../../components/Contact/Contact'
+import Contact from '../../components/Contact/Contact'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
