53a952e967b57d15abc07b0b89556d6968e32477
HeleneHuang <helene568363@gmail.com>
2025-06-23 13:20:14 +0200

    finished products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+// import { useState } from "react";
+// import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
+// import useFetch from "../../hooks/useFetch";
+import "./Products.scss";
 
 const Products = () => {
-    return (
-        <div>Products</div>
-    )
-}
+    // const catId = parseInt(useParams().id);
+    // const [maxPrice, setMaxPrice] = useState(1000);
+    // const [sort, setSort] = useState(null);
+    // const [selectedSubCats, setSelectedSubCats] = useState([]);
 
-export default Products
+    // const { data, loading, error } = useFetch(
+    //     `/sub-categories?[filters][categories][id][$eq]=${catId}`
+    // );
+
+    // const handleChange = (e) => {
+    //     const value = e.target.value;
+    //     const isChecked = e.target.checked;
+
+    //     setSelectedSubCats(
+    //         isChecked
+    //             ? [...selectedSubCats, value]
+    //             : selectedSubCats.filter((item) => item !== value)
+    //     );
+    // };
+
+    return (
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    {/* {data.map((item) => ( */}
+                    <div className="inputItem">
+                        <input
+                            type="checkbox"
+                            id="1"
+                            value={1}
+                        // onChange={handleChange}
+                        />
+                        <label htmlFor="1">skirt</label>
+                    </div>
+                    {/* ))} */}
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input
+                            type="range"
+                            min={0}
+                            max={1000}
+                        // onChange={(e) => setMaxPrice(e.target.value)}
+                        />
+                        <span>1000</span>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input
+                            type="radio"
+                            id="asc"
+                            value="asc"
+                            name="price"
+                        // onChange={(e) => setSort("asc")}
+                        />
+                        <label htmlFor="asc">Price (Lowest first)</label>
+                    </div>
+                    <div className="inputItem">
+                        <input
+                            type="radio"
+                            id="desc"
+                            value="desc"
+                            name="price"
+                        // onChange={(e) => setSort("desc")}
+                        />
+                        <label htmlFor="desc">Price (Highest first)</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="right">
+                <img
+                    className="catImg"
+                    src="assets/img/cart.webp"
+                    alt=""
+                />
+                {/* <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} /> */}
+            </div>
+        </div>
+    );
+};
+
+export default Products;
